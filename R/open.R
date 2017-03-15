35c6363bdbee73409cf5b3a03c7b6866af1e6e76
JARS3N <JARS3N@users.noreply.github.com>
2017-03-15 15:53:10 -0400

    Update open.R

--- R/open.R
+++ R/open.R
@@ -1,6 +1,6 @@
 
 open<-function(){
-shiny::runApp(system.file("move",package="in"),
+shiny::runApp(system.file("move",package="door"),
               launch.browser=FALSE,
               host="0.0.0.0",
               port=9999
