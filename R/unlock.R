5f1a2e00e9edf0761bef96bfc4ed0ffa41fe9111
JARS3N <JARS3N@users.noreply.github.com>
2019-05-14 11:42:55 -0400

    Update unlock.R

--- R/unlock.R
+++ R/unlock.R
@@ -1,6 +1,6 @@
 
 unlock<-function(){
-shiny::runApp(system.file("move",package="door"),
+shiny::runApp(shiny::shinyApp(move_ui(),move_server()),
               launch.browser=FALSE,
               host="0.0.0.0",
               port=9999
