52acc14c47ba4da73f0c1430ed907ca13b3a564b
JARS3N <JARS3N@users.noreply.github.com>
2019-05-14 11:41:19 -0400

    Create move_ui.R

---
+++ R/move_ui.R
@@ -0,0 +1,18 @@
+
+move_ui<-function(){
+library(shiny)
+shinyUI(fluidPage(
+  titlePanel(h4('File Transfer')),
+  mainPanel(
+    fileInput("files", "File data", multiple=TRUE),
+    fluidRow(width="50%",
+             column(width=2,p("User:")),
+             column(width=2,textOutput('nm'))
+    ),
+    fluidRow(width="100%",
+             column(width=2,p("Machine:")),
+             column(width=2,textOutput('cmp'))
+    )
+  )
+))
+}
