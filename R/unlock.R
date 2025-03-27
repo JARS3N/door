d70824ac231dbfd2ccafba58706bc65d8ee15734
JARS3N <JARS3N@users.noreply.github.com>
2025-03-27 11:19:21 -0400

    Update unlock.R

--- R/unlock.R
+++ R/unlock.R
@@ -1,8 +1,10 @@
 
 unlock<-function(){
+  sys<-Sys.info()
 shiny::runApp(shiny::shinyApp(move_ui(),move_server()),
               launch.browser=FALSE,
               host="0.0.0.0",
               port=9999
               )
+   message( paste0("Listening on http://",sys['nodename'],":9999"))
 }
