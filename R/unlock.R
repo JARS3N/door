5042dbf7985dd7dfc3e8c4f01f5adeab5e991eaf
JARS3N <JARS3N@users.noreply.github.com>
2025-03-27 11:37:56 -0400

    Update unlock.R

--- R/unlock.R
+++ R/unlock.R
@@ -1,10 +1,10 @@
 
 unlock<-function(){
-
+hostname <- Sys.info()[['nodename']]
+message("Unlocking ... http://",hostname,":9999")
 shiny::runApp(shiny::shinyApp(move_ui(),move_server()),
               launch.browser=FALSE,
-             # host="0.0.0.0",
-              host = Sys.info()[["nodename"]],
+              host="0.0.0.0",
               port=9999
               )
   }
